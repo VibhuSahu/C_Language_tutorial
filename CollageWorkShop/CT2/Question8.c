c7f6ae01c266520dec5a624388e6726278a2c2e0
Vibhu Sahu <vibhu6751@gmail.com>
2023-11-06 22:39:47 +0530

    Final push of the day

--- CollageWorkShop/CT2/Question8.c
+++ CollageWorkShop/CT2/Question8.c
@@ -1,24 +1,36 @@
 // # QUES 8: wap to perform simple interest using function with argument and  no return data type 
 #include<stdio.h>
 
-void simple_int(float p,float r,float t);
+void simple_int1(float p,float r,float t);
+float simple_int2(float p,float r,float t);
 
-void main() {
+int main() {
     float a,b,c;
     
-    printf("enter the principle");
+    printf("enter   the   principle    : ");
     scanf("%f",&a);
-    printf("enter the rate of interest");
+    printf("enter the rate of interest : ");
     scanf("%f",&b);
-    printf("enter the time");
+    printf("enter   the     time       : ");
     scanf("%f",&c);
-    simple_int(a,b,c);
+    simple_int1(a,b,c);
 
+    float interest = simple_int2(a,b,c);
+    printf("simple interest=%.2f\n",interest);
     
+
+    return 0;
 }
 
-void simple_int(float p,float r,float t){
+void simple_int1(float p,float r,float t){
     float si;
     si=(p*r*t)/100;
-    printf("simple interest=%.2f",si);
+    printf("simple interest=%.2f\n",si);
+}
+
+float simple_int2(float p,float r,float t)
+{
+    float si;
+    si=(p*r*t)/100;
+    return si;
 }
